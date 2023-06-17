4a085a08df45f93af26ef9333a6ad2f27359e895
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-06-17 20:08:48 +0900

    wip

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const fs =  require("fs");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+      "Content-Type": "text/html"
+    });
+	res.end()
+})
