1501aa682168ffc06e6600b8eb0f94dbb61326a5
ihasq <gsd79d2mtizifb7cv8d119a9xpq3ja@gmail.com>
2023-06-28 13:27:05 +0900

    wip

--- js/export.mjs
+++
@@ -1,5 +0,0 @@
-import * as Adapter from "./module/adapter.mjs"
-import * as Buffer from "./module/buffer.mjs"
-import * as Surface from "./module/surface.mjs"
-
-export default { Adapter, Buffer, Surface }
